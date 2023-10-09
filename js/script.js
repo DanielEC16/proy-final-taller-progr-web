d422118b440d66a7859b82b6672251740d57f11b
DanielEC16 <danielcairo695@gmail.com>
2023-10-09 02:46:28 -0500

    Cambios

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Inicializa Swiper
-    var swiper = new Swiper(".swiper-container", {
+
+    var swiper = new Swiper(".swiper-container-1", {
         effect: "fade",
         loop: true,
         autoplay: {
@@ -8,3 +8,23 @@ document.addEventListener("DOMContentLoaded", function () {
         },
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".swiper-container-2", {
+        slidesPerView: 6,
+        spaceBetween: 1,
+    });
+});
+
+
+
+const sede = document.querySelectorAll('.sede')
+sede.forEach(function(elemento){
+    elemento.addEventListener('click',function(){
+        sede.forEach(function(item){
+            item.classList.remove('sede')
+        })
+        elemento.classList.add('sede-active')
+    })
+})
